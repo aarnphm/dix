c7472f6c299e84aa64bccf6a903a44d624bbb946
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-02-04 05:41:15 -0500

    chore: add darwin nix configs
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

---
+++ .nixpkgs/darwin-configuration.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+
+let
+    go = pkgs.callPackage ./go.nix {};
+in
+{
+  nixpkgs.config.allowUnfree = true;
+  environment.variables = { EDITOR = "neovim"; };
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+  [
+    pkgs.neovim
+    pkgs.vim
+    pkgs.tmux
+    pkgs.jdk
+    pkgs.jre
+    pkgs.openjdk17
+    pkgs.asciinema
+    pkgs.ccls
+    pkgs.fd
+    pkgs.fzf
+    pkgs.git
+    pkgs.gnupg
+    go
+    pkgs.jq
+    pkgs.llvm
+    pkgs.ninja
+    pkgs.tree
+    pkgs.wget
+    pkgs.zip
+  ];
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
