122e057332f0196cf516e02acf2e7436adbcda30
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-02-04 06:07:59 -0500

    chore: add packages
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-    go = pkgs.callPackage ./go.nix {};
+  go = pkgs.callPackage ./go.nix { };
 in
 {
   nixpkgs.config.allowUnfree = true;
@@ -9,35 +9,37 @@ in
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages =
-  [
-    pkgs.neovim
-    pkgs.vim
-    pkgs.tmux
-    pkgs.jdk
-    pkgs.jre
-    pkgs.openjdk17
-    pkgs.asciinema
-    pkgs.ccls
-    pkgs.fd
-    pkgs.fzf
-    pkgs.git
-    pkgs.gnupg
-    go
-    pkgs.jq
-    pkgs.llvm
-    pkgs.ninja
-    pkgs.tree
-    pkgs.wget
-    pkgs.zip
-    pkgs.pstree
-  ];
+    [
+      pkgs.neovim
+      pkgs.vim
+      pkgs.tmux
+      pkgs.jdk
+      pkgs.jre
+      pkgs.openjdk17
+      pkgs.asciinema
+      pkgs.ccls
+      pkgs.fd
+      pkgs.fzf
+      pkgs.git
+      pkgs.gnupg
+      go
+      pkgs.jq
+      pkgs.llvm
+      pkgs.ninja
+      pkgs.tree
+      pkgs.wget
+      pkgs.zip
+      pkgs.pstree
+      pkgs.enchant
+      pkgs.vscode
+    ];
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   # nix.package = pkgs.nix;
 
   # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
+  programs.zsh.enable = true; # default shell on catalina
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
