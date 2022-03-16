a57acc3f57b55daf02edb8c6265f410c29a6debd
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-03-16 13:39:21 -0400

    feat: adding flakes (#1)

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -2,12 +2,14 @@
 
 let
   go = pkgs.callPackage ./go.nix { };
+  postgresql = pkgs.postgresql_14;
 in
 {
   nixpkgs.config.allowUnfree = true;
   environment.variables = { EDITOR = "neovim"; };
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
+  # nix-env -i /nix/store/ws2fbwf7xcmnhg2hlvq43qg22j06w5jb-niv-0.2.19-bin --option binary-caches https://cache.nixos.org
   environment.systemPackages =
     [
       pkgs.neovim
@@ -23,7 +25,6 @@ in
       pkgs.git
       pkgs.gnupg
       # golang
-      /* go */
       pkgs.jq
       pkgs.llvm
       pkgs.ninja
@@ -41,21 +42,21 @@ in
       pkgs.openssl_3_0
       pkgs.colima
       pkgs.skopeo
+      pkgs.nnn
       # kubernetes
       pkgs.minikube
       pkgs.kubernetes
       pkgs.kubernetes-helm
-      pkgs.nnn
       pkgs.ngrok
       pkgs.buildkit
-      pkgs.postgresql_14
+      postgresql
       pkgs.direnv
       pkgs.nixFlakes
     ];
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
+  nix.package = pkgs.nix;
 
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.zsh.enable = true; # default shell on catalina
@@ -65,6 +66,6 @@ in
   system.stateVersion = 4;
 
   services.postgresql.enable = true;
-  services.postgresql.package = pkgs.postgresql_14;
+  services.postgresql.package = postgresql;
   services.postgresql.dataDir = "/data/postgresql";
 }
