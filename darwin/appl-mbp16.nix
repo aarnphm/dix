aaafba79d255c5cc57f877c9882bd2ce9138d30a
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2024-06-10 22:48:29 -0400

    chore: move trusted public key to flake
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- darwin/appl-mbp16.nix
+++ darwin/appl-mbp16.nix
@@ -22,8 +22,6 @@ in
     auto-optimise-store = true;
     experimental-features = "nix-command flakes";
     trusted-users = [ "root" "aarnphm" ];
-    trusted-substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
-    trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
   };
 
   nix.gc = {
--- flake.nix
+++ flake.nix
@@ -60,6 +60,11 @@
       packages.aarch64-darwin.dix = darwin-pkgs.dix;
       packages.x86_64-linux.dix = linux-pkgs.dix;
 
+      nixConfig = {
+        trusted-substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
+        trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+      };
+
       darwinConfigurations = {
         appl-mbp16 = nix-darwin.lib.darwinSystem rec {
           system = "aarch64-darwin";
