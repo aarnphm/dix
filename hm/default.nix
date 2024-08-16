40174702878abeea30fb3a5889e4ec7eaf9ebf71
Aaron Pham <contact@aarnphm.xyz>
2024-08-16 15:39:58 -0400

    fix: make sure to avoid newlines
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- hm/default.nix
+++ hm/default.nix
@@ -298,11 +298,9 @@ in {
         (
           if config.home.sessionVariables.XDG_SYSTEM_THEME == "dark"
           then ''
-            --color=fg:#908caa,bg:#191724,hl:#ebbcba --color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba --color=border:#403d52,header:#31748f,gutter:#191724 --color=spinner:#f6c177,info:#9ccfd8 --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa
-          ''
+            --color=fg:#908caa,bg:#191724,hl:#ebbcba --color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba --color=border:#403d52,header:#31748f,gutter:#191724 --color=spinner:#f6c177,info:#9ccfd8 --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa''
           else ''
-            --color=fg:#797593,bg:#faf4ed,hl:#d7827e --color=fg+:#575279,bg+:#f2e9e1,hl+:#d7827e --color=border:#dfdad9,header:#286983,gutter:#faf4ed --color=spinner:#ea9d34,info:#56949f --color=pointer:#907aa9,marker:#b4637a,prompt:#797593
-          ''
+            --color=fg:#797593,bg:#faf4ed,hl:#d7827e --color=fg+:#575279,bg+:#f2e9e1,hl+:#d7827e --color=border:#dfdad9,header:#286983,gutter:#faf4ed --color=spinner:#ea9d34,info:#56949f --color=pointer:#907aa9,marker:#b4637a,prompt:#797593''
         )
         ''
           --bind='ctrl-/:toggle-preview'
